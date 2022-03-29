6bfc8ced501b0b3c84c0173eba0082e139d68e10
eswaribala <parameswaribala@gmail.com>
2022-03-29 15:47:14 +0530

    updated

--- inventoryservice/src/main/java/com/eric/inventoryservice/controllers/CatalogController.java
+++ inventoryservice/src/main/java/com/eric/inventoryservice/controllers/CatalogController.java
@@ -1,8 +1,12 @@
 package com.eric.inventoryservice.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +31,35 @@ public class CatalogController {
     	else
     		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Catalog not created");	
     	}
+    
+    
+    //select all
+    @GetMapping({"/v1.0"})
+    public List<Catalog> getAllCatalogs(){
+    	return this.catalogService.getAllCatalogs();
     }
+    
+    
+    //select by id
+    @GetMapping({"/v1.0/{catalogId}"})
+    public ResponseEntity<?> getCatalogById(@PathVariable("catalogId") long catalogId) {
+    	 
+    	Catalog catalogObj=this.catalogService.getCatalogById(catalogId);
+    	if(catalogObj!=null) {
+    		return ResponseEntity.status(HttpStatus.CREATED).body(catalogObj);
+    	}
+    	else
+    	{
+    		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Catalog not found");	
+    	}
+    }
+    
+    
+    
+    }
+
+
+   
     
 	
 	
