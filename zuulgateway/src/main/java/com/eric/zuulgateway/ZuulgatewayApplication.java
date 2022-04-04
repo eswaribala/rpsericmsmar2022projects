c9ecdeb2e68cfc303f79d4f7335470b17cf44ddb
eswaribala <parameswaribala@gmail.com>
2022-04-04 15:18:34 +0530

    updated

--- zuulgateway/src/main/java/com/eric/zuulgateway/ZuulgatewayApplication.java
+++ zuulgateway/src/main/java/com/eric/zuulgateway/ZuulgatewayApplication.java
@@ -2,8 +2,13 @@ package com.eric.zuulgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
 public class ZuulgatewayApplication {
 
 	public static void main(String[] args) {
