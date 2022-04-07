ace2ae353592b33158fbf171c2286644c34f60cf
eswaribala <parameswaribala@gmail.com>
2022-04-07 14:45:45 +0530

    updated

---
+++ circuitbreakerapi/src/main/java/com/eric/circuitbreakerapi/CircuitbreakerapiApplication.java
@@ -0,0 +1,13 @@
+package com.eric.circuitbreakerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitbreakerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitbreakerapiApplication.class, args);
+	}
+
+}
---
+++ circuitbreakerapi/src/test/java/com/eric/circuitbreakerapi/CircuitbreakerapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eric.circuitbreakerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitbreakerapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
