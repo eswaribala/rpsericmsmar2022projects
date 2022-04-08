37ca1002fcf1637d8dcc587c055b54b061bba9b9
eswaribala <parameswaribala@gmail.com>
2022-04-08 14:28:59 +0530

    updated

---
+++ ribbonlb/src/test/java/com/eric/ribbonlb/RibbonlbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eric.ribbonlb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibbonlbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
